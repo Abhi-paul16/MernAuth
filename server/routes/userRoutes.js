53e1174748ef033fb0fe170bd524c943ddebe3aa
Abhi-paul16 <abhishek1100861@gmail.com>
2023-11-04 23:19:35 +0530

    changing the code

--- server/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { authUser ,registerUser , getUserProfile , updateUserProfile , logoutUser } from '../controllers/userController.js';
-const router = express.Router();
- 
- router.post('/' , registerUser);
- router.post('/auth' , authUser);
- router.post('/logout' , logoutUser);
- router.route('/profile').get(getUserProfile).put(updateUserProfile)
- export default router;
